28a157e7b1ac5e59df3f584cc9a54e14c4d7ef9e
wessmch <mecherguiwassim@gmail.com>
2016-11-15 17:45:11 +0100

    Signed-off-by: wessmch <mecherguiwassim@gmail.com>

--- src/com/video2brain/javase/Launcher.java
+++ src/com/video2brain/javase/Launcher.java
@@ -3,6 +3,7 @@ package com.video2brain.javase;
 public class Launcher {
 	
 public static void main (String[] args) {
-System.out.println("Hello World");
+System.out.println("Hello World!!!!!!!");
 }
+
 }
